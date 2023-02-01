e463fd1e9bc2b91c73a1ed5c477cea277c6d968d
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-01 18:48:08 +0100

    Add key to show

--- WatchMe/Models/Show.cs
+++ WatchMe/Models/Show.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WatchMe.Models
 {
-
     public class Show
     {
+        [Key]
         public string Title { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
