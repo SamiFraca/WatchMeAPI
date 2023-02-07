90d266a415f1b2f762e479f53b85d1c8af79870b
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-07 18:45:57 +0100

    Change bar controller for dbContext

--- WatchMe/Controllers/BarsController.cs
+++ WatchMe/Controllers/BarsController.cs
@@ -1,6 +1,9 @@
 using WatchMe.Models;
 using WatchMe.Services;
 using Microsoft.AspNetCore.Mvc;
+using WatchMe.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace WatchMe.Controllers
 {
@@ -9,57 +12,91 @@ namespace WatchMe.Controllers
     [Route("[controller]")]
     public class BarsController : ControllerBase
     {
-        //  public static List<Bar> allbar = Bar.GetUsersFromJSON();
-
+         private readonly DataContext _dbContext;
         private readonly ILogger<BarsController> _logger;
-        public BarsController()
+        public BarsController(DataContext dbContext)
         {
+            _dbContext = dbContext;
         }
 
         [HttpGet]
-        public ActionResult<List<Bar>> GetAll() =>
-         BarService.GetAll();
-        [HttpGet("{id}")]
-        public ActionResult<Bar> Get(int id)
+        public async Task<ActionResult<List<Bar>>> GetBars()
         {
-            var Bar = BarService.Get(id);
-
-            if (Bar == null)
+            if (_dbContext.Bars == null)
+            {
                 return NotFound();
+            }
+            return await _dbContext.Bars.ToListAsync();
+        }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bar>> GetBar(int id)
+        {
+            if (_dbContext.Bars == null)
+            {
+                return NotFound();
+            }
+            var Bar = await _dbContext.Bars.FindAsync(id);
+            if (Bar == null)
+            {
+                return NotFound();
+            }
             return Bar;
         }
         [HttpPost]
-        public IActionResult Create(Bar Bar)
+        public async Task<ActionResult<Bar>> PostBar(Bar Bar)
         {
-            BarService.Add(Bar);
-            return CreatedAtAction(nameof(Get), new { id = Bar.Id }, Bar);
+            _dbContext.Bars.Add(Bar);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBar), new { id = Bar.Id }, Bar);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Update(int id, Bar Bar)
+        public async Task<IActionResult> PutBar(int id, Bar Bar)
         {
             if (id != Bar.Id)
+            {
                 return BadRequest();
-
-            var existingBar = BarService.Get(id);
-            if (existingBar is null)
-                return NotFound();
-
-            BarService.Update(Bar);
-
+            }
+            _dbContext.Entry(Bar).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
             return NoContent();
         }
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+
+        private bool BarExists(long id)
         {
-            var Bar = BarService.Get(id);
+            return (_dbContext.Bars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
 
-            if (Bar is null)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBar(int id)
+        {
+            if (_dbContext.Bars is null)
+            {
                 return NotFound();
+            }
 
-            BarService.Delete(id);
-
+            var Bar = await _dbContext.Bars.FindAsync(id);
+            if (Bar == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Bars.Remove(Bar);
+            await _dbContext.SaveChangesAsync();
             return NoContent();
         }
     }
