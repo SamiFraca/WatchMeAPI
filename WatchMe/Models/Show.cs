ff8b28bec3d6efc3c42b44ea33d595c3aedcd383
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-02-08 18:45:28 +0100

    Add Id to show model

--- WatchMe/Models/Show.cs
+++ WatchMe/Models/Show.cs
@@ -4,7 +4,7 @@ namespace WatchMe.Models
 {
     public class Show
     {
-        [Key]
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
