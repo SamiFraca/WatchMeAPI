68a1dc3739c282efdd180186f3a7109daac3742f
Sami Fraca <fracasami@gmail.com>
2023-03-12 17:57:53 +0100

    Add swagger schema for datetimes

--- Models/Show.cs
+++ Models/Show.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+using Newtonsoft.Json;
 
 namespace WatchMe.Models
 {
@@ -6,9 +8,25 @@ namespace WatchMe.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        [JsonIgnore]
         public DateTime Start { get; set; }
+
+        [JsonIgnore]
         public DateTime End { get; set; }
+
+        [SwaggerSchema("MM/dd HH:mm")]
+        public string StartTime
+        {
+            get { return Start.ToString("MM/dd HH:mm"); }
+        }
+
+        [SwaggerSchema("MM/dd HH:mm")]
+        public string EndTime
+        {
+            get { return End.ToString("MM/dd HH:mm"); }
+        }
         public int BarId { get; set; }
-        public string Sport { get;set; }
+        public string Sport { get; set; }
     }
 }
