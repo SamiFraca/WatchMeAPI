7a0921dd94c2182cc35c21a6c19e6b489ec1c0c7
SamiFraca <sami.fraca@flat101.es>
2023-06-01 17:31:21 +0200

    Add storage service + connectionstring

---
+++ Services/AzureBlobStorageService.cs
@@ -0,0 +1,37 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+
+public class AzureBlobStorageService
+{
+    private readonly BlobServiceClient _blobServiceClient;
+    private readonly string _containerName;
+
+    public AzureBlobStorageService(IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("AzureBlobStorage:ConnectionString");
+        _containerName = configuration["AzureBlobStorage:ContainerName"];
+        _blobServiceClient = new BlobServiceClient(connectionString);
+    }
+
+    public async Task<string> UploadImageAsync(string fileName, Stream imageStream)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        await blobClient.UploadAsync(imageStream, overwrite: true);
+
+        return blobClient.Uri.ToString();
+    }
+
+    public async Task<Stream> GetImageAsync(string fileName)
+    {
+        BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+        BlobDownloadInfo downloadInfo = await blobClient.DownloadAsync();
+        return downloadInfo.Content;
+    }
+}
