140bfe2cf00c98f98dc0556ec46919255e0f4594
Sami Fraca <fracasami@gmail.com>
2023-06-01 20:20:41 +0200

    Add show capacity migration record

---
+++ Migrations/20230601182000_CapacityShows.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchMe.Migrations
+{
+    /// <inheritdoc />
+    public partial class CapacityShows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "actualCap",
+                table: "Shows",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "maxCap",
+                table: "Shows",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "actualCap",
+                table: "Shows");
+
+            migrationBuilder.DropColumn(
+                name: "maxCap",
+                table: "Shows");
+        }
+    }
+}
